bcf54adb5485934b3162e84ad4f0180c4ced3a31
Agrim Choudhary <agrimchoadhary@gmail.com>
2025-10-31 16:18:32 +0530

    2

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,13 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  // Generated code to prevent build breaking APIs.
+  // It is called automatically by the Flutter tool as needed.
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+@end
